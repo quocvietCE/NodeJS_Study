89f9f8ddfe62234d31a25fb56e65a28117356cc3
viet.nguyen.successsoftware <viet.nguyen@successsoftware.global>
2020-05-06 13:08:21 +0700

    Bài 26 - HTTP API

--- NodeJS_Web_Server_1/controllers/product.controller.js
+++ NodeJS_Web_Server_1/controllers/product.controller.js
@@ -1,7 +1,7 @@
 // const db = require('../db);
 const Product = require('../models/product.model');
 
-module.exports.index = (req,res) => {
+module.exports.index = async (req,res) => {
 	// res.render('products/index', {
 	// 	const page = parseInt(req.query.page) || 1;
 	// 	const perPage = 10;
@@ -13,9 +13,14 @@ module.exports.index = (req,res) => {
 	// 	products: db.get('products').drop(drop).take(perPage).value();
 	// });
 
-	Product.find().then((products) => {
-		res.render('products/index', {
-			products: products
-		});
+	// constProduct.find().then((products) => {
+	// 	res.render('products/index', {
+	// 		products: products
+	// 	});
+	// });
+
+	const products = await Product.find();
+	res.render('products/index', {
+		products: products
 	});
-}
+};
--- NodeJS_Web_Server_1/index.js
+++ NodeJS_Web_Server_1/index.js
@@ -10,8 +10,11 @@ mongoose.connect(process.env.MONGO_URL);
 
 const userRoute = require('./routes/user.route');
 const authRoute = require('./routes/auth.route');
+const productRoute = require('./routes/product.route');
 
 const authMiddleware = require('./middlewares/auth.middleware');
+
+const apiProductRoute = require('./api/routes/product.route');
 
 const port = 3000;
 
@@ -28,8 +31,10 @@ app.use(express.static('public')); // read file static
 
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
+app.use('/api/products', apiProductRoute);
 
 app.get('/', (req, res) => res.render('index', {name: 'aaa'}))
+
 
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
